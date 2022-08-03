64e28f9664b2857587abe5314355c1f3bf8c02f7
Antti Sandberg <antti.sandberg@gmail.com>
2022-08-03 16:22:41 +0300

    Update site title, fix JSON load

--- src/views/items.tsx
+++ src/views/items.tsx
@@ -4,7 +4,7 @@ import '../App.css';
 import Dropzone from '../components/dropzone';
 import { Item, ItemElement } from '../components/item';
 import ItemForm from '../components/addItemForm';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 
 // Parent view for adding, viewing and deleting items.
@@ -12,6 +12,8 @@ function Items(props: any) {
 
     const [a, setA] = useState<boolean>(true);
     const [b, setB] = useState<boolean>(true);
+
+    const loadField = useRef<HTMLInputElement>(null);
 
     // Import items from a JSON file
     const handleSetFile = (file: Blob) => {
@@ -35,6 +37,19 @@ function Items(props: any) {
         a.href = URL.createObjectURL(file);
         a.download = "YouSort Set.json";
         a.click();
+    }
+
+    const uploadJson = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files === null)
+            return;
+        let reader = new FileReader();
+        reader.onloadend = function () {
+            if (props.getItems() !== null && props.getItems().length > 0)
+                props.setItems([...props.getItems(), ...JSON.parse(reader.result as string)]);
+            else
+                props.setItems([...JSON.parse(reader.result as string)]);
+        }
+        reader.readAsText(e.target.files![0]);
     }
 
     // Get new unique ID for a new item.
@@ -63,10 +78,11 @@ function Items(props: any) {
                 {/*<Dropzone fileHandler={handleSetFile}></Dropzone>*/}
             </div>
             <div id="itemListOptions">
+                <input type="file" onChange={uploadJson} ref={loadField}></input>
                 <h1>Items</h1>
                 <div>
                     <button className="onBlack" onClick={downloadJSON}>Save</button>
-                    <button className="onBlack" onClick={downloadJSON}>Load</button>
+                    <button className="onBlack" onClick={() => loadField.current!.click()}>Load</button>
                 </div>
             </div>
             {props.getItems().sort(orderByID).map((item: Item) => {
